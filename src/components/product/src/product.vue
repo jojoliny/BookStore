cc0f4a16d5f9514a5eddf3ac5be0296b62f251f8
jojoliny <370795317@qq.com>
2021-07-08 00:50:51 +0800

    first generate

---
+++ src/components/product/src/product.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <el-card shadow="hover" :body-style="{ padding: '20px' }">
+        <div slot="header">
+            <span><!-- card title --></span>
+        </div>
+        <!-- card body -->
+    </el-card>
+  </div>
+</template>
